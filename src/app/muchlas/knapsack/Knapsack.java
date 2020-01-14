e5ccf2ac756e71a20532f1919347ae17b3b97614
muchlaseen <muchlaseen@gmail.com>
2020-01-14 07:06:12 +0700

    Update the main app

--- src/app/muchlas/knapsack/Knapsack.java
+++ src/app/muchlas/knapsack/Knapsack.java
@@ -61,17 +61,5 @@ public class Knapsack {
 
     return new Solution(itemsSolution, matrix[NB_ITEMS][capacity]);
   }
-
-  public static void main(String[] args) {
-    Item[] items = {new Item("Elt1", 4, 12), 
-	                new Item("Elt2", 2, 1), 
-					new Item("Elt3", 2, 2), 
-					new Item("Elt4", 1, 1),
-                    new Item("Elt5", 10, 4)};
-
-    Knapsack knapsack = new Knapsack(items, 15);
-    knapsack.display();
-    Solution solution = knapsack.solve();
-    solution.display();
-  }
+  
 }
